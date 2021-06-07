fc1bca6bdec3e5dfecd45280fbe2c5f210ffc1ba
ScodroS <sebastianoscodro@gmail.com>
2021-06-07 17:23:28 +0200

    augmentation main fixes

--- augmentation.py
+++ augmentation.py
@@ -12,9 +12,6 @@ def augment(image, prob=0.2):
     :param image: rgb image (dtype uint8)
     :return: augmented image (dtype uint8)
     """
-    # shape = (image.shape[0], image.shape[1])
-    probability = int(random.random() * 100)
-    # only 50% of the images will be affected by data augmentation
 
     transform = A.Compose([
         A.RandomBrightnessContrast(p=prob),
@@ -29,15 +26,7 @@ def augment(image, prob=0.2):
 
     return image
 
-def convert(img, target_type_min, target_type_max, target_type):
-    imin = img.min()
-    imax = img.max()
-
-    a = (target_type_max - target_type_min) / (imax - imin)
-    b = target_type_max - a * imax
-    new_img = (a * img + b).astype(target_type)
-    return new_img
-
+# Showing how the augmentation is done (5 images augmented with their original counterparts
 if __name__ == "__main__":
 
     with open(os.path.join('dataset', 'set_imgs'), 'rb') as imgs:
@@ -47,23 +36,27 @@ if __name__ == "__main__":
     columns = 5
     fig = plt.figure()
     ax = []
+
     # 5 augmented images to show how the augmentation is performed (original and augmented below)
 
     for i in range(columns):
 
         p = random.random() * 100 // 1
 
-        if p < 50:
-            dataset[i] = cv2.flip(dataset[i], -1)
-
         ax.append(fig.add_subplot(rows, columns, i + 1))
         image = dataset[i]
         plt.imshow(image.astype(np.uint8))
         plt.axis('off')
+
+        if p < 50:
+            dataset[i] = cv2.flip(dataset[i], -1)
 
         ax.append(fig.add_subplot(rows, columns, columns + i + 1))
         image = augment(dataset[i], prob=0.4)
         plt.imshow(image.astype(np.uint8))
         plt.axis('off')
 
+    fig.suptitle('Augmentation examples', fontsize=16)
+    ax[4].set_title('Original images')
+    ax[5].set_title('Augmented images')
     plt.show()
