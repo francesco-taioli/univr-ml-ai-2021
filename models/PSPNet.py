28201de4b05cbcb8650b01c206c262f17d882910
francesco <taioli.francesco98@gmail.com>
2021-06-22 14:40:00 +0200

    add pspnet

--- models/PSPNet.py
+++ models/PSPNet.py
@@ -1,14 +1,16 @@
 from segmentation_models import PSPNet
-
+from utils import get_env_variable
 # define model
 # todo change input size
 class PSP_Net():
     def __init__(self):
+        self.WIDTH = int(get_env_variable('WIDTH'))
+        self.HEIGHT = int(get_env_variable('HEIGHT'))
         self.BACKBONE = 'resnet34'
 
     def get_model(self):
         return  PSPNet(self.BACKBONE,
-                          input_shape=(256, 256, 3),
+                          input_shape=(self.HEIGHT, self.WIDTH, 3),
                           classes=3,
                           encoder_weights='imagenet'
                           )
