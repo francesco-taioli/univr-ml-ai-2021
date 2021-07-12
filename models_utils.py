41b93d540078ecc4b24820a9e883ffde857432fb
francesco <taioli.francesco98@gmail.com>
2021-07-12 14:58:26 +0200

    refactor

--- models_utils.py
+++ models_utils.py
@@ -4,24 +4,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from skimage import color
-from tensorflow.keras import layers
 from utils import get_env_variable
-
-
-def tversky_loss(y_true, y_pred):
-    alpha = 0.5
-    beta = 0.5
-    ones = K.ones(K.shape(y_true))
-    p0 = y_pred  # proba that voxels are class i
-    p1 = ones - y_pred  # proba that voxels are not class i
-    g0 = y_true
-    g1 = ones - y_true
-    num = K.sum(p0 * g0, (0, 1, 2, 3))
-    den = num + alpha * K.sum(p0 * g1, (0, 1, 2, 3)) + beta * K.sum(p1 * g0, (0, 1, 2, 3))
-    T = K.sum(num / den)  # when summing over classes, T has dynamic range [0 Ncl]
-    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
-
-    return Ncl - T
 
 
 def pixel_wise_loss():
@@ -50,7 +33,6 @@ def weighted_categorical_crossentropy():
         return K.categorical_crossentropy(y_true, y_pred) * K.sum(y_true * Kweights, axis=-1)
 
     return wcce
-
 
 
 def mean_IoU(y_true, y_pred):
